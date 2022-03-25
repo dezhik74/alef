f2aaeed4c06090bcd7154470f7e06e381fd8765c
Serje <dezhik@yandex.ru>
2022-03-25 14:01:02 +0300

    deploy gh (2)

--- src/utils/getDataFromAPI.js
+++ src/utils/getDataFromAPI.js
@@ -1,17 +1,16 @@
 import leftImage from "../assets/img/boy-car.png";
+import t1 from "../assets/img/thumbs/t-1.png";
+import t2 from "../assets/img/thumbs/t-2.png";
+import t3 from "../assets/img/thumbs/t-3.png";
+import t4 from "../assets/img/thumbs/t-4.png";
+import t5 from "../assets/img/thumbs/t-5.png";
 
 export default async function getDataFromAPI() {
-  // let promise = new Promise((resolve) => {
-  //   setTimeout(() => resolve(), 1000);
-  // });
-  // await promise;
-
   let data = {
     thumbs: [],
     leftImage: leftImage,
   };
-  for (let img in import.meta.glob("/src/assets/img/thumbs/*.png"))
-    data.thumbs.push(img);
+  for (let img of [t1, t2, t3, t4, t5]) data.thumbs.push(img);
   data.name = "Пижама для девочек";
   data.articul = "02765/46";
   data.stars = 4;
@@ -22,6 +21,6 @@ export default async function getDataFromAPI() {
   data.actionPercent = 20;
   data.sizes = ["XS", "S", "M", "XXXL"];
 
-  // console.log(data);
+  // console.log(data.thumbs);
   return data;
 }
